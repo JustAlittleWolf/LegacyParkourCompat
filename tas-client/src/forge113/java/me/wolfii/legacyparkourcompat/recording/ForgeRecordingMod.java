4a91e0858a7e3883daa3254cb5993e0e6d4462d5
JustAlittleWolf <jakobi5000@gmail.com>
2026-09-23 04:57:34 +0200

    Use Forge events for 1.13.2 TAS hooks

--- tas-client/src/forge113/java/me/wolfii/legacyparkourcompat/recording/ForgeRecordingMod.java
+++ tas-client/src/forge113/java/me/wolfii/legacyparkourcompat/recording/ForgeRecordingMod.java
@@ -1,6 +1,11 @@
 package me.wolfii.legacyparkourcompat.recording;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.ClientChatEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -8,6 +13,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public final class ForgeRecordingMod {
     public ForgeRecordingMod() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(FMLCommonSetupEvent event) {
@@ -17,6 +23,33 @@ public final class ForgeRecordingMod {
             controller.configureAutomation(AutomationSettings.fromSystemProperties());
         } catch (IllegalArgumentException exception) {
             System.err.println("[Legacy Parkour Recording] Invalid automation setting: " + exception.getMessage());
+        }
+    }
+
+    @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.START) {
+            RecordingController.get().clientTick();
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.player != Minecraft.getInstance().player) {
+            return;
+        }
+        RecordingController controller = RecordingController.get();
+        if (event.phase == TickEvent.Phase.START) {
+            controller.beforePlayerTick();
+        } else {
+            controller.afterPlayerTick();
+        }
+    }
+
+    @SubscribeEvent
+    public void onClientChat(ClientChatEvent event) {
+        if (RecordingController.get().handleCommand(event.getMessage())) {
+            event.setCanceled(true);
         }
     }
 }
